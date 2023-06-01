2e2d35521908e6d909a3d7109961036acac020dd
Luis Leguizamón <lleguizamonl@gmail.com>
2023-06-01 19:42:22 -0400

    Adjust: Center all content, add margins and paddings and separate in divs applying bootstrap

--- index.php
+++ index.php
@@ -9,56 +9,67 @@
 </head>
 <body>
     <!-- Presentation -->
-    <div>
-        <h1>Luis Leguizamón</h1>
-        <h2>Full Stack Developer</h2>
+    <div class="container my-2">
+        <div class="p-5 text-center">
+            <h1>Luis Leguizamón</h1>
+            <h2>Full Stack Developer</h2>
+            <a href="#contact" class="btn btn-outline-secondary rounded-pill">Contact</a>
+        </div>
     </div>
 
     <!-- Tech Stack -->
-    <div>
-        <h2>Tech Stack</h2>
-        <h2>PHP | Laravel | Vue JS | Javascript</h2>
+    <div class="container">
+        <div class="p-5 text-center">
+            <h2>Tech Stack</h2>
+            <h2>PHP | Laravel | Vue JS | Javascript</h2>
+        </div>
     </div>
 
     <!-- Works -->
-    <div>
-        <h2>Works</h2>
-        <ul>
-            <h2>Open Source Projects</h2>
-            <li>
-                <a href="/https://taskmanager.zentcode.com/">Task Manager</a>
-            </li>
-            <li>
-                <a href="https://courses.zentcode.com/">Video Courses</a>
-            </li>
-            <li>
-                <a href="https://github.com/zentcodepy/mailgun-logs-by-domain">Mailgun Logs</a>
-            </li>
-            <h2>Experience</h2>
-            <li>
-                SASS ERP
-            </li>
-            <li>
-                Land Managment
-            </li>
-            <li>
-                GYM Manager and integrations
-            </li>
-            <li>
-                Request for Quotation (RFQ)
-            </li>
-            <li>
-                Shopping mall mobile App
-            </li>
-        </ul>
+    <div class="container">
+        <div class="p-5 text-center">
+            <h2>Works</h2>
+            <h2 class="mt-2">Open Source Projects</h2>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <a href="https://taskmanager.zentcode.com/">Task Manager</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="https://courses.zentcode.com/">Video Courses</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="https://github.com/zentcodepy/mailgun-logs-by-domain">Mailgun Logs</a>
+                </li>
+            </ul>
+            <h2 class="mt-3">Commercial Projects</h2>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    SASS ERP
+                </li>
+                <li class="list-group-item">
+                    Land Managment
+                </li>
+                <li class="list-group-item">
+                    GYM Manager and integrations
+                </li>
+                <li class="list-group-item">
+                    Request for Quotation (RFQ)
+                </li>
+                <li class="list-group-item">
+                    Shopping mall mobile App
+                </li>
+            </ul>
+        </div>
     </div>
 
     <!-- Contact -->
-    <div>
-        <h2>Contact</h2>
-        <a href="https://www.linkedin.com/in/luis-leguizamonl/">Linkedin</a>
-        |
-        <a href="mailto:lleguizamonl@gmail.com">lleguizamonl@gmail.com</a>
+    <div id="contact" class="container">
+        <div class="p-5 text-center">
+            <h2>Contact</h2>
+            <a href="https://www.linkedin.com/in/luis-leguizamonl/">Linkedin</a>
+            <br>
+            <a href="mailto:lleguizamonl@gmail.com">lleguizamonl@gmail.com</a>
+        </div>
     </div>
 </body>
 </html>

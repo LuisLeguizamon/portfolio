120855436738b376554ef1dfaa1af0ee9206f100
Luis Leguizamón <lleguizamonl@gmail.com>
2023-06-01 18:49:00 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Luis Leguizamón</title>
+</head>
+<body>
+    <!-- Presentation -->
+    <div>
+        <h1>Luis Leguizamón</h1>
+        <h2>Full Stack Developer</h2>
+    </div>
+
+    <!-- Tech Stack -->
+    <div>
+        <h2>Tech Stack</h2>
+        <h2>PHP | Laravel | Vue JS | Javascript</h2>
+    </div>
+
+    <!-- Works -->
+    <div>
+        <h2>Works</h2>
+        <ul>
+            <h2>Open Source Projects</h2>
+            <li>
+                <a href="/https://taskmanager.zentcode.com/">Task Manager</a>
+            </li>
+            <li>
+                <a href="https://courses.zentcode.com/">Video Courses</a>
+            </li>
+            <li>
+                <a href="https://github.com/zentcodepy/mailgun-logs-by-domain">Mailgun Logs</a>
+            </li>
+            <h2>Experience</h2>
+            <li>
+                SASS ERP
+            </li>
+            <li>
+                Land Managment
+            </li>
+            <li>
+                GYM Manager and integrations
+            </li>
+            <li>
+                Request for Quotation (RFQ)
+            </li>
+            <li>
+                Shopping mall mobile App
+            </li>
+        </ul>
+    </div>
+
+    <!-- Contact -->
+    <div>
+        <h2>Contact</h2>
+        <a href="https://www.linkedin.com/in/luis-leguizamonl/">Linkedin</a>
+        |
+        <a href="mailto:lleguizamonl@gmail.com">lleguizamonl@gmail.com</a>
+    </div>
+</body>
+</html>

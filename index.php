a2c6d9be156af171ce3db4750a6271b0ebd4086c
Luis Leguizamón <lleguizamonl@gmail.com>
2023-06-02 15:39:18 -0400

    Adjust: Add bootstrap js to make the menu responsive and move all menu content inside a container to add a space

--- index.php
+++ index.php
@@ -14,24 +14,26 @@
 <body>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg fixed-top navbarScroll">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" aria-current="page" href="#home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#tech-stack">Tech Stack</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#works">Works</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#contact">Contact</a>
-                </li>
-            </ul>
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#tech-stack">Tech Stack</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#works">Works</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
@@ -114,6 +116,8 @@
             </div>
         </div>
     </div>
+
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>

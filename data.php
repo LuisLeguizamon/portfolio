7fd4799a983acc04109baa3dcfecc75a7a21e85b
Luis Leguizamón <lleguizamonl@gmail.com>
2023-06-02 00:10:36 -0400

    Refactor: Separate data and move it to another php file

---
+++ data.php
@@ -0,0 +1,18 @@
+<?php
+$openSourceProjects = [
+    'Task Manager' => 'https://taskmanager.zentcode.com',
+    'Video Courses' => 'https://courses.zentcode.com/',
+    'Mailgun Logs' => 'https://github.com/zentcodepy/mailgun-logs-by-domain'
+];
+
+$comercialProjects = [
+    'Gym Management and integration with facial detection access system',
+    ' Web platform for gym trainers',
+    'Shopping mall mobile App',
+    'Request for Quotation (RFQ)',
+    'Land Management',
+    'ERP SASS'
+];
+
+$techStack = ['Laravel', 'Vue JS', 'PHP', 'JavaScript'];
+?>

afe2cf4ad2ca83281b66433ed2294baab4c31e2b
Luis Leguizamón <lleguizamonl@gmail.com>
2023-06-05 13:46:43 -0400

    Adjust: Navbar toggle button style

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 
 <head>
-    <title>Portfolio - Luis Leguizamón</title>
+    <title>Portfolio - Luis LeguizamÃ³n</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +15,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg fixed-top navbarScroll">
         <div class="container">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="border:none">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">

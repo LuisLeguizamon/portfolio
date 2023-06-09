0577351b54ecdc0a8d30a13d9e90699c0e0d34e7
Luis Leguizamón <lleguizamonl@gmail.com>
2023-06-09 17:14:03 -0400

    Adjust: Move script code that executes when DOM is loaded to the top

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let spinnerSection = document.getElementById("spinnerSection");
+  spinnerSection.remove();
+
+  let bannerImg = document.getElementById("bannerImg");
+  let bannerContactBtn = document.getElementById("bannerContactBtn");
+  let bannerSecondaryBtn = document.getElementById("bannerSecondaryBtn");
+  let opacity = 0;
+  let fadeIn = setInterval(() => {
+    bannerImg.style.opacity = opacity;
+    bannerContactBtn.style.opacity = opacity;
+    bannerSecondaryBtn.style.opacity = opacity;
+    opacity += 0.01;
+    if (opacity >= 1) {
+      clearInterval(fadeIn);
+    }
+  }, 10);
+});
+
 let navbar = document.getElementById("navbarSupportedContent");
 window.onscroll = function (event) {
     if (window.scrollY > 0) {
@@ -6,25 +25,6 @@ window.onscroll = function (event) {
         navbar.classList.remove('navbar-scrolled');
     }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    let spinnerSection = document.getElementById("spinnerSection");
-    spinnerSection.remove();
-
-    let bannerImg = document.getElementById("bannerImg");
-    let bannerContactBtn = document.getElementById("bannerContactBtn");
-    let bannerSecondaryBtn = document.getElementById("bannerSecondaryBtn");
-    let opacity = 0;
-    let fadeIn = setInterval(() => {
-      bannerImg.style.opacity = opacity;
-      bannerContactBtn.style.opacity = opacity;
-      bannerSecondaryBtn.style.opacity = opacity;
-      opacity += 0.01;
-      if (opacity >= 1) {
-        clearInterval(fadeIn);
-      }
-    }, 10);
-});
 
 function openLink(url) {
   window.open(url, '_blank');

095702ca38355dc8297fc4274a04b0bfbe798390
Luis Leguizamón <lleguizamonl@gmail.com>
2023-08-05 12:30:12 -0400

    Refactor: Add Contact class for contact data

---
+++ data/Contact.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace data;
+
+class Contact
+{
+    public $author = 'Luis Leguizamón';
+
+    public $mail = 'lleguizamonl@gmail.com';
+
+    public $links = [
+        'github' => 'https://github.com/luisleguizamon',
+        'linkedin' => 'https://www.linkedin.com/in/luis-leguizamonl',
+    ];
+}
--- data/data.php
+++ data/data.php
@@ -1,5 +1,6 @@
 <?php
 
+use data\Contact;
 use data\Project;
 use data\TechStack;
 use data\Tool;
@@ -7,8 +8,10 @@ use data\Tool;
 include('data/Project.php');
 include('data/TechStack.php');
 include('data/Tool.php');
+include('data/Contact.php');
 
 $project = new Project();
 $techStack = new TechStack();
 $tool = new Tool();
+$contact = new Contact();
 ?>

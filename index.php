e0e8a63a6a3bce04b5c0fa1e285b8ec08804b735
Luis Leguizamón <lleguizamonl@gmail.com>
2023-11-02 14:32:00 -0300

    Adjust: Remove animation from main banner

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css?v=3.0">
+    <link rel="stylesheet" href="style.css?v=4.0">
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-P5B7JBXXZ3"></script>
     <script>
@@ -78,13 +78,13 @@
 
             </div>
         </div>
-        <div class="col-md-6 text-center banner-right" id="bannerImg">
+        <div class="col-md-6 text-center banner-right">
         </div>
     </section>
 
     <!-- Tech Stack -->
     <section id="tech-stack" class="container my-5">
-        <div class="animation p-5 text-center bg-body-tertiary rounded-3">
+        <div class="p-5 text-center bg-body-tertiary rounded-3">
             <h3>Main Tech Stack</h3>
             <div class="stack-row text-secondary">
                 <?php foreach ($techStack->techStack as $key => $stack) { ?>
--- src/script/script.js
+++ src/script/script.js
@@ -2,24 +2,24 @@ window.addEventListener("load", () => {
   let spinnerSection = document.getElementById("spinnerSection");
   spinnerSection.remove();
 
-  let bannerImg = document.getElementById("bannerImg");
-  let bannerContactBtn = document.getElementById("bannerContactBtn");
-  let bannerSecondaryBtn = document.getElementById("bannerSecondaryBtn");
-  let opacity = 0;
-  let scale = 0.8;
-  let fadeIn = setInterval(() => {
-    // Opacity
-    bannerImg.style.opacity = opacity;
-    bannerContactBtn.style.opacity = opacity;
-    bannerSecondaryBtn.style.opacity = opacity;
-    opacity += 0.01;
-    // Scale
-    bannerImg.style.transform = `scale(${scale})`;
-    scale += 0.002;
-    if (opacity >= 1) {
-      clearInterval(fadeIn);
-    }
-  }, 10);
+  // let bannerImg = document.getElementById("bannerImg");
+  // let bannerContactBtn = document.getElementById("bannerContactBtn");
+  // let bannerSecondaryBtn = document.getElementById("bannerSecondaryBtn");
+  // let opacity = 0;
+  // let scale = 0.8;
+  // let fadeIn = setInterval(() => {
+  //   // Opacity
+  //   bannerImg.style.opacity = opacity;
+  //   bannerContactBtn.style.opacity = opacity;
+  //   bannerSecondaryBtn.style.opacity = opacity;
+  //   opacity += 0.01;
+  //   // Scale
+  //   bannerImg.style.transform = `scale(${scale})`;
+  //   scale += 0.002;
+  //   if (opacity >= 1) {
+  //     clearInterval(fadeIn);
+  //   }
+  // }, 10);
 });
 
 let navbar = document.getElementById("navbarSupportedContent");

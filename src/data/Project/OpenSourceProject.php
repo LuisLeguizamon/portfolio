0160bc6ecbab0bfdcb86478024fd9ba0207c6965
Luis Leguizamón <lleguizamonl@gmail.com>
2023-11-09 10:21:36 -0300

    Adjust: Remove open source projects

--- src/data/Project/OpenSourceProject.php
+++ src/data/Project/OpenSourceProject.php
@@ -6,8 +6,6 @@ class OpenSourceProject implements ProjectInterface
     public function getProjects(): array
     {
         $openSourceProjects = [
-            'Task Manager' => 'https://taskmanager.zentcode.com',
-            'Video Courses' => 'https://courses.zentcode.com/',
             'Mailgun Logs' => 'https://github.com/zentcodepy/mailgun-logs-by-domain',
             'Whatsapp Widget Plugin' => 'https://github.com/LuisLeguizamon/whatsapp-widget-plugin',
             'Subscription Backend' => 'https://github.com/zentcodepy/subscriptions-manager-backend',
